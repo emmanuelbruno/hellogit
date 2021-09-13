2fef9928f024d91814ade0a7909d19ccff21562e
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2021-09-13 13:38:13 +0200

    sets initial release.

---
+++ src/main/java/fr/univtln/bruno/po43/App.java
@@ -0,0 +1,13 @@
+package fr.univtln.bruno.po43;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
